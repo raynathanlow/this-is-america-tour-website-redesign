6576f747edb635b24ee2f63888f34f968b8d715f
Ray Nathan Low <raynathanlow@gmail.com>
2018-10-08 14:03:25 -0700

    Updates

---
+++ precache-manifest.4b4d49113b0e62676ad1dc2665a0b916.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e89e81b30e685b21a400",
+    "url": "/this-is-america-tour-website-redesign/static/js/runtime~main.e89e81b3.js"
+  },
+  {
+    "revision": "374ca951ec98ee1c6270",
+    "url": "/this-is-america-tour-website-redesign/static/js/main.374ca951.chunk.js"
+  },
+  {
+    "revision": "186431d67b63dcc17c21",
+    "url": "/this-is-america-tour-website-redesign/static/js/1.186431d6.chunk.js"
+  },
+  {
+    "revision": "374ca951ec98ee1c6270",
+    "url": "/this-is-america-tour-website-redesign/static/css/main.83b3e0fe.chunk.css"
+  },
+  {
+    "revision": "524c7c601a2d0e424dc77183ef523cdc",
+    "url": "/this-is-america-tour-website-redesign/index.html"
+  }
+];
