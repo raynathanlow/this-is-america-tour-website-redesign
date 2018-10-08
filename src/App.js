e0a3f0b9bbbc8c4ca746993a34836b6f4749d811
Ray Nathan Low <raynathanlow@gmail.com>
2018-10-08 11:16:33 -0700

    Fix Get Tickets title to top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
 import React, { Component } from "react";
 import Shows from "./Shows";
 
+var h2 = document.querySelector('h2');
+var h2Pos = getPosition(h2);
+
+document.body.onscroll = function () {
+  if (window.pageYOffset >= h2Pos) {
+    h2.classList.add('fixed');
+  } else {
+    h2.classList.remove('fixed');
+  }
+};
+
+window.addEventListener = function () {
+  if (window.pageYOffset >= h2Pos) {
+    h2.classList.add('fixed');
+  } else {
+    h2.classList.remove('fixed');
+  }
+};
+
+function getPosition(element) {
+  var yPosition = 0;
+
+  while (element) {
+    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    element = element.offsetParent;
+  }
+
+  return yPosition;
+}
+
 class App extends Component {
   render() {
     return <Shows />;
