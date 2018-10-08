5370bc7b8e0da577ad95b82db2497a48169db582
Ray Nathan Low <raynathanlow@gmail.com>
2018-10-08 11:25:47 -0700

    Fix how Get Tickets title fixes to top Prior to this change, the Get Tickets title would not allow the window to stay scrolled to the bottom of the page. Also the event listener added to the window was not complete. 'resize' was missing so that the code used to fix the title to the top would also execute when the window is resized.
    This change now allows the page to scroll down to the bottom of the page
    and stays there. Also, the title will fix to the top when the window is
    resized.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
 import Shows from "./Shows";
 
-var h2 = document.querySelector('h2');
-var h2Pos = getPosition(h2);
+var h2 = document.querySelector("h2");
+var h2Pos = getPosition(h2) + 6;
 
-document.body.onscroll = function () {
+document.body.onscroll = function() {
   if (window.pageYOffset >= h2Pos) {
-    h2.classList.add('fixed');
+    h2.classList.add("fixed");
   } else {
-    h2.classList.remove('fixed');
+    h2.classList.remove("fixed");
   }
 };
 
-window.addEventListener = function () {
+window.addEventListener("resize", () => {
   if (window.pageYOffset >= h2Pos) {
-    h2.classList.add('fixed');
+    h2.classList.add("fixed");
   } else {
-    h2.classList.remove('fixed');
+    h2.classList.remove("fixed");
   }
-};
+});
 
 function getPosition(element) {
   var yPosition = 0;
 
   while (element) {
-    yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+    yPosition += element.offsetTop - element.scrollTop + element.clientTop;
     element = element.offsetParent;
   }
 
